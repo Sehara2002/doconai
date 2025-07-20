bee69472f5f8d0b1684f4f4db4f0f5f1f8e56b30
Sehara2002 <shehara1010@gmail.com>
2025-07-20 12:34:07 +0530

    Updating the Main

--- backend/services/userService.py
+++
@@ -1,6 +0,0 @@
-from db.database import db
-from schemas.userSchema import UserCreate
-
-async def create_user(user: UserCreate):
-    result = await db.users.insert_one(user.dict())
-    return str(result.inserted_id)
